0ecf8ce878aad32d8efc4752179144d6a6b61175
John Coogan <141885470+johncoogan53@users.noreply.github.com>
2024-08-25 16:46:05 +0000

    remove target from source control

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,9 +2,11 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+
     let keyword_string: &String = args.get(1).expect("No argument given");
-    println!("The provided argument is: {}",keyword_string);
     let file_path: &String = args.get(2).expect("No file path given");
+
+    println!("The provided argument is: {}",keyword_string);
     println!("The provided file path is: {}",file_path);
+
 }
