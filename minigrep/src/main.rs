3a4797ff47eb87cbce226b5c9203004f71ccaab5
John Coogan <141885470+johncoogan53@users.noreply.github.com>
2024-08-25 16:42:48 +0000

    target updates

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,8 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //dbg!(&args);
-    let second_arg: &String = args.get(1).expect("No argument given");
-    println!("The provided argument is: {}",second_arg);
-
+    dbg!(&args);
+    let keyword_string: &String = args.get(1).expect("No argument given");
+    println!("The provided argument is: {}",keyword_string);
+    let file_path: &String = args.get(2).expect("No file path given");
+    println!("The provided file path is: {}",file_path);
 }
