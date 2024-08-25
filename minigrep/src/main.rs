7ca81f4c7df622ce8e74dbb97e6f5c3680bf24dd
John Coogan <141885470+johncoogan53@users.noreply.github.com>
2024-08-25 18:23:28 +0000

    struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,12 +1,25 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let keyword_string: &String = args.get(1).expect("No argument given");
-    let file_path: &String = args.get(2).expect("No file path given");
+    let config: Config = parse_config(&args);
 
-    println!("Searching for keyword: {}",keyword_string);
-    println!("In File: {}",file_path);
+    let contents = fs::read_to_string(&config.file_path).expect(&format!("File at {} should exist",&config.file_path));
 
+    println!("Searching for: {}",config.query);
+    println!("File Contents: \n{}",contents);
+
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String])-> Config {
+    let query: String = args.get(1).expect("There should be a first argument to the CLI").clone();
+    let file_path: String = args.get(2).expect("There should be a second argument to the CLI").clone();
+    Config{query,file_path}
 }
