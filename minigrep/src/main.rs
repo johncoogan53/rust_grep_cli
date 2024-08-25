05c546a4ff7064f80c94faf81855533cf1fab0a5
John Coogan <141885470+johncoogan53@users.noreply.github.com>
2024-08-25 16:48:45 +0000

    try again to rm target from source

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let keyword_string: &String = args.get(1).expect("No argument given");
     let file_path: &String = args.get(2).expect("No file path given");
 
-    println!("The provided argument is: {}",keyword_string);
-    println!("The provided file path is: {}",file_path);
+    println!("Searching for: {}",keyword_string);
+    println!("In File: {}",file_path);
 
 }
