e2af444fff0cebd50143c850f27dd81e0c28041d
John Coogan <141885470+johncoogan53@users.noreply.github.com>
2024-08-27 00:17:41 +0000

    impl struct abstraction

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config: Config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents = fs::read_to_string(&config.file_path).expect(&format!("File at {} should exist",&config.file_path));
 
@@ -18,8 +18,11 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String])-> Config {
-    let query: String = args.get(1).expect("There should be a first argument to the CLI").clone();
-    let file_path: String = args.get(2).expect("There should be a second argument to the CLI").clone();
-    Config{query,file_path}
+impl Config {
+    fn new(args: &[String])->Config { //argument signature could be Vec<String> but dereference coercion in Rust
+        //makes &Vec<T> automatically the slice &[String] to prevent ownership.  
+        let query: String = args.get(1).expect("There should be a first argument to the CLI").clone();
+        let file_path: String = args.get(2).expect("There should be a second argument to the CLI").clone();
+        Config{query,file_path}
+    }
 }
