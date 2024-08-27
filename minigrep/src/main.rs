18e0ebe9644ca4c560ff32ef06fea09314fc90e6
John Coogan <141885470+johncoogan53@users.noreply.github.com>
2024-08-27 00:31:07 +0000

    fmt

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,11 +6,11 @@ fn main() {
 
     let config = Config::new(&args);
 
-    let contents = fs::read_to_string(&config.file_path).expect(&format!("File at {} should exist",&config.file_path));
+    let contents = fs::read_to_string(&config.file_path)
+        .expect(&format!("File at {} should exist", &config.file_path));
 
-    println!("Searching for: {}",config.query);
-    println!("File Contents: \n{}",contents);
-
+    println!("Searching for: {}", config.query);
+    println!("File Contents: \n{}", contents);
 }
 
 struct Config {
@@ -19,10 +19,17 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String])->Config { //argument signature could be Vec<String> but dereference coercion in Rust
-        //makes &Vec<T> automatically the slice &[String] to prevent ownership.  
-        let query: String = args.get(1).expect("There should be a first argument to the CLI").clone();
-        let file_path: String = args.get(2).expect("There should be a second argument to the CLI").clone();
-        Config{query,file_path}
+    fn new(args: &[String]) -> Config {
+        //argument signature could be Vec<String> but dereference coercion in Rust
+        //makes &Vec<T> automatically the slice &[String] to prevent ownership.
+        let query: String = args
+            .get(1)
+            .expect("There should be a first argument to the CLI")
+            .clone();
+        let file_path: String = args
+            .get(2)
+            .expect("There should be a second argument to the CLI")
+            .clone();
+        Config { query, file_path }
     }
 }
